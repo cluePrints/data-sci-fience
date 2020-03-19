d27011d49e90c71e03ca71b5bd4c9bfbd6de24b0
cluePrints <cluePrints@users.noreply.github.com>
2020-03-19 05:50:43 -0700

    Re-extract item series plot

--- m5-forecasting-accuracy/ivanocode/ivanocommon.py
+++ m5-forecasting-accuracy/ivanocode/ivanocommon.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 def plot_item_series(df, itemid):
     subset = df.query(f'id == "{itemid}"')
     sales = subset['sales'].values
+    prices = None
+    prices_cols = []
+    if 'sell_price' in subset:
+        prices = subset['sell_price'].values
+        prices_legend = ['prices']
     days = subset['day_id'].values
     stats = f"daily_avg: {subset[0:1]['daily_avg_count'].unique()}\n"+\
             f"month_avg: {subset[0:1]['monthly_avg_count'].unique()}\n"+\
@@ -12,5 +17,8 @@ def plot_item_series(df, itemid):
     fig, ax = plt.subplots(figsize=(20,5))
     ax.hlines(y=daily_avg, xmin=days[0], xmax=days[-1], colors='r', linestyles='--', lw=2)
     ax.plot(days, sales)
-    ax.legend(['series', 
-               stats])
+    if prices is not None:
+        ax.plot(days, prices)
+    ax.legend(['series']+
+              prices_legend+
+               [stats])
