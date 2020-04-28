1611260c022a25dfe702041a52b5f82eb2e7b3c3
cluePrints <cluePrints@users.noreply.github.com>
2020-04-28 08:36:17 -0700

    Fix: Mindless filtering of zeroes breaks wrmsse denom where all the sales were of the same counts

--- m5-forecasting-accuracy/ivanocode/pipeline/pipeline.py
+++ m5-forecasting-accuracy/ivanocode/pipeline/pipeline.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import os
+from functools import partial
 
 raw = os.environ.get('DATA_RAW_DIR', 'raw')
 processed = 'processed'
@@ -28,20 +29,23 @@ lr_find          = bool(os.environ.get('LR_FIND', '').lower() != 'false')
 
 dataloader_num_workers = None
 
-import wandb
-from wandb.fastai import WandbCallback
-wandb.init(
-    project='kaggle-m5-accuracy',
-    reinit=True
-)
-wandb.config.update({
-    "epochs": n_train_epochs,
-    "batch_size": batch_size,
-    "n_trn_days": trn_days,
-    "n_trn_series": n_sample_series,
-    "lr": lr
-})
-wandb_callback = partial(WandbCallback)
+def init_wandb():
+    import wandb
+    from wandb.fastai import WandbCallback
+    wandb.init(
+        project='kaggle-m5-accuracy',
+        reinit=True
+    )
+    wandb.config.update({
+        "epochs": n_train_epochs,
+        "batch_size": batch_size,
+        "n_trn_days": trn_days,
+        "n_trn_series": n_sample_series,
+        "lr": lr
+    })
+    return partial(WandbCallback)
+
+wandb_callback = init_wandb()
 
 def reproducibility_mode():
     global dataloader_num_workers
@@ -64,7 +68,28 @@ def reproducibility_mode():
     import numpy as np
     np.random.seed(seed)
 
+
+import time
+def timeit(log_time = None):
+    def decorator(method):
+        def wrapper(*args, **kw):
+            ts = time.time()
+            result = method(*args, **kw)
+            te = time.time()
+            time_spent = int((te - ts) * 1000)
+            if log_time is not None:
+                name = kw.get('log_name', method.__name__.upper())
+                log_time[name] = time_spent
+            else:
+                print(f'{method.__name__}  {time_spent:.2f} ms')
+            return result
+        return wrapper
+    return decorator
+
+timings = {}
+
 @memory.cache
+@timeit(log_time = timings)
 def read_series_sample(n = 10):
     indexes = np.arange(1, n_total_series + 1)
     np.random.shuffle(indexes)
@@ -76,6 +101,7 @@ def read_series_sample(n = 10):
     return pd.read_csv(f'{raw}/sales_train_validation.csv', skiprows = lambda i: i not in sample_idx)
 
 @memory.cache
+@timeit(log_time = timings)
 def melt_sales_series(df_sales_train):
     id_columns = [col for col in df_sales_train.columns if 'id' in col]
     sales_columns = [col for col in df_sales_train.columns if 'd_' in col]
@@ -95,6 +121,7 @@ def melt_sales_series(df_sales_train):
     return df_sales_train_melt
 
 @memory.cache
+@timeit(log_time = timings)
 def extract_day_ids(df_sales_train_melt):
     sales_columns = [f'd_{col}' for col in range(1, n_days_total+1)]
     mapping = {col: int(col.split('_')[1]) for col in sales_columns}
@@ -116,6 +143,7 @@ def extract_day_ids(df_sales_train_melt):
     return df_sales_train_melt
 
 @memory.cache
+@timeit(log_time = timings)
 def join_w_calendar(df_sales_train_melt):
     df_calendar = pd.read_csv(f'{raw}/calendar.csv')
     df_sales_train_melt =  df_sales_train_melt.merge(
@@ -127,6 +155,7 @@ def join_w_calendar(df_sales_train_melt):
     return df_sales_train_melt
 
 @memory.cache
+@timeit(log_time = timings)
 def join_w_prices(partition):
     df_prices = pd.read_csv(f'{raw}/sell_prices.csv')
     partition = partition.merge(
@@ -210,14 +239,21 @@ class SingleBatchProgressTracker(LearnerCallback):
         return {'last_metrics': last_metrics + [score]}
 
 def model_as_tabular(df_sales_train_melt):
-    non_zero = df_sales_train_melt.query('sales_dollars > 0').reset_index(drop=True)
-    valid_idx = np.flatnonzero(non_zero['day_id'] > trn_days)
+    prefiltered = df_sales_train_melt.reset_index(drop=True)
+    valid_idx = np.flatnonzero(prefiltered['day_id'] > trn_days)
 
     # TODO: this in fact is fragile and won't work without reset index above, can I do this differently?
-    val_mask = non_zero.index.isin(valid_idx)
-    val = non_zero[val_mask]
-    trn = non_zero[~val_mask]
-    print(f"sample: {len(df_sales_train_melt)} non_zero: {len(non_zero)} trn: {len(trn)} val: {len(val)}")
+    val_mask = prefiltered.index.isin(valid_idx)
+    val = prefiltered[val_mask]
+    trn = prefiltered[~val_mask]
+    print(f"sample: {len(df_sales_train_melt)} prefiltered: {len(prefiltered)} trn: {len(trn)} val: {len(val)}")
+
+    import wandb
+    wandb.log({
+        "trn_examples": len(df_sales_train_melt),
+        "trn_examples_filtered": len(prefiltered),
+        "val_examples": len(val)
+    })
 
     my_metrics_cb = partial(MyMetrics, val=val, trn=trn)
     single_batch_metric = partial(SingleBatchProgressTracker, metric=rmse, metric_name='example_batch_rmse')
@@ -228,7 +264,7 @@ def model_as_tabular(df_sales_train_melt):
     cols = cat_names + ['sell_price'] + [dep_var]
 
     path = tmp_dir
-    data = TabularDataBunch.from_df(path, non_zero[cols], dep_var, valid_idx=valid_idx,
+    data = TabularDataBunch.from_df(path, prefiltered[cols], dep_var, valid_idx=valid_idx,
                                     bs=batch_size,
                                     num_workers=dataloader_num_workers,
                                     procs=procs, cat_names=cat_names)
@@ -246,7 +282,7 @@ def model_as_tabular(df_sales_train_melt):
         learn.lr_find()
         fig = learn.recorder.plot(return_fig=True)
         fig.savefig('lr_find.png')
-        !open lr_find.png
+        # TODO: !open lr_find.png
     learn.fit_one_cycle(n_train_epochs, lr)
     fig = learn.recorder.plot_losses(return_fig=True)
     fig.savefig('loss_log.png')
@@ -278,6 +314,7 @@ def extract_id_columns(t):
     return t
 
 @memory.cache
+@timeit(log_time = timings)
 def get_submission_template_melt(df_sales_train_melt):
     df_sample_submission = pd.read_csv('raw/sample_submission.csv')
     df_sample_submission.head()
@@ -299,6 +336,7 @@ def get_submission_template_melt(df_sales_train_melt):
     df_sample_submission_melt = extract_id_columns(df_sample_submission_melt)
     return df_sample_submission_melt
 
+@timeit(log_time = timings)
 def to_submission(learn, df_sample_submission_melt):
     from datetime import timedelta
     submission = df_sample_submission_melt.pivot(index='id', columns='day', values='sales').reset_index()
@@ -318,6 +356,12 @@ def to_submission(learn, df_sample_submission_melt):
     
     return submission
 
+def push_timings_to_wandb():
+    import wandb
+    wandb.log({
+        f'time_{k}': v for (k,v) in timings.items()
+    })
+    
 def run_pipeline():
     # TODO: all of this preprocessing ought to happen once and than sampling can use that final dataframe
     reproducibility_mode()
@@ -333,5 +377,7 @@ def run_pipeline():
     if do_submit:
         submission = to_submission(learn, submission_template)
         submission.to_csv(f'{tmp_dir}/0500-fastai-pipeline.csv', index=False)
+        
+    push_timings_to_wandb()
         
     return learn, trn, val
